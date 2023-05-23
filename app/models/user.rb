3b52a4356f9242b2d0660e13fd543c1a23e87675
Harry Fleminger <harry.fleminger@hotmail.com>
2023-05-23 17:17:39 +0100

    Created blog_post model, controller, and routes. Created pages controller, homepage+about views and routes. Created migrations for user+devise and migrations for blogpost. Added the blog/user associations and basic validations.

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_many :blog_posts, dependent: :destroy
+  validates :first_name, :last_name, presence: true
+  validates :email, presence: true
+end
