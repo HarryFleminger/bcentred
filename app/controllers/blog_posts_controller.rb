1f2cd6d0851796fd163b44be019aa801531e5762
Harry Fleminger <harry.fleminger@hotmail.com>
2023-05-26 18:26:21 +0100

    Properly configured cloudinary. Added show page for blog posts. Added initial cards to blog index page

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -44,6 +44,6 @@ class BlogPostsController < ApplicationController
   private
 
   def blog_post_params
-    params.require(:blog_post).permit(:title, :body, photo: [])
+    params.require(:blog_post).permit(:title, :body, :photo)
   end
 end
